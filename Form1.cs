5ec071f7dd000e4b59c6ce33048fc78cd68a40f2
sxg037 <43061685+sxg037@users.noreply.github.com>
2018-10-23 10:56:31 +0800

    V1.6版本。在配置文件中增加了两款水晶板的码

--- Form1.cs
+++ Form1.cs
@@ -30,9 +30,9 @@ namespace _3VJ_MV
         {
             InitializeComponent();
 
-            #region 设软件小工具版本号V1.5  宋新刚电脑是读取本地D:\模板忽删里的配置文件，其他电脑读取1.20服务器上面的
+            #region 设软件小工具版本号V1.6  宋新刚电脑是读取本地D:\模板忽删里的配置文件，其他电脑读取1.20服务器上面的
 
-            string currentversion = "V1.5";
+            string currentversion = "V1.6";
 
             IniFiles inifile_First = new IniFiles(Path.Combine(Environment.CurrentDirectory, "OrderNo.ini"));
             if (inifile_First.ExistINIFile())
